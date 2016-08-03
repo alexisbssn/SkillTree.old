aa95cfbc4896643dfbc410b4114d84095516d6a5
alexis.buisson@hotmail.com <alexis.buisson@hotmail.com>
2016-08-02 23:08:45 -0400

    added copy zone, fixed dialogs

--- demo/nested/nested.js
+++ demo/nested/nested.js
@@ -1,4 +1,5 @@
 angular.module("skilltree").controller("AdminPanelController", function ($scope, $mdDialog) {
+    
     $scope.models = {
         selected: null,
         templates: [
@@ -8,83 +9,85 @@ angular.module("skilltree").controller("AdminPanelController", function ($scope,
         ],
         ruleTypes: [
             {data: "", label: "-- Select a rule type --"},
-            {data: "gt", label: "has more than ... taken"},
-            {data: "lt", label: "has less than ... taken"},
-            {data: "eq", label: "has equal to ... taken"},
-            {data: "cst", label: "costs"}
+            {data: "gt", label: "has more than ... taken in target"},
+            {data: "lt", label: "has less than ... taken in target"},
+            {data: "eq", label: "has equal to ... taken in target"},
+            {data: "cst", label: "costs ... target"}
         ],
         dropzones: {
-            "Skills": [
+            Skills: [
                 {
-                    "type": "group",
-                    "title": "Magic",
-                    "items": [
+                    type: "group",
+                    title: "Magic",
+                    items: [
                         {
-                            "type": "skill",
-                            "title": "Fireball",
-                            "rules": [
+                            type: "skill",
+                            title: "Fireball",
+                            rules: [
                                 {
-                                    "type": "rule",
-                                    "title": "test rule 1"
+                                    type: "rule",
+                                    title: "test rule 1"
                                 },
                                 {
-                                    "type": "rule",
-                                    "title": "test rule 1"
+                                    type: "rule",
+                                    title: "test rule 2"
                                 }
                             ]
                         },
                         {
-                            "type": "skill",
-                            "title": "Frostbite",
-                            "rules": []
+                            type: "skill",
+                            title: "Frostbite",
+                            rules: []
                         },
                         {
-                            "type": "skill",
-                            "title": "Entangle",
-                            "rules": []
+                            type: "skill",
+                            title: "Entangle",
+                            rules: []
                         },
                         {
-                            "type": "skill",
-                            "title": "Magic missile",
-                            "rules": []
+                            type: "skill",
+                            title: "Magic missile",
+                            rules: []
                         }
                     ]
                 },
                 {
-                    "type": "group",
-                    "title": "Combat",
-                    "items": [
+                    type: "group",
+                    title: "Combat",
+                    items: [
                         {
-                            "type": "skill",
-                            "title": "Parry",
-                            "rules": []
+                            type: "skill",
+                            title: "Parry",
+                            rules: []
                         },
                         {
-                            "type": "skill",
-                            "title": "Lunge",
-                            "rules": []
+                            type: "skill",
+                            title: "Lunge",
+                            rules: []
                         },
                         {
-                            "type": "skill",
-                            "title": "Slash",
-                            "rules": []
+                            type: "skill",
+                            title: "Slash",
+                            rules: []
                         }
                     ]
                 },
                 {
-                    "type": "skill",
-                    "title": "Health",
-                    "rules": []
+                    type: "skill",
+                    title: "Health",
+                    rules: []
                 }
             ]
         }
     };
+    
+    $scope.copyZone = [];
 
     $scope.$watch('models.dropzones', function (model) {
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
 
-    $scope.editSkill = function () {
+    $scope.edit = function () {
         $mdDialog.show({
             controller: function DialogController($scope, $mdDialog) {
                 $scope.hide = function () {
@@ -94,7 +97,7 @@ angular.module("skilltree").controller("AdminPanelController", function ($scope,
                     $mdDialog.cancel();
                 };
             },
-            templateUrl: 'dialog_skill.html',
+            templateUrl: 'editDialog.html',
             parent: angular.element(document.body),
             scope: $scope,
             preserveScope: true,
@@ -103,37 +106,6 @@ angular.module("skilltree").controller("AdminPanelController", function ($scope,
             //$scope.status = 'You said the information was "' + answer + '".';
         }, function () {
             //$scope.status = 'You cancelled the dialog.';
-        });
-    };
-
-    $scope.editRule = function (rule) {
-        $scope.selectedRule = rule;
-        $mdDialog.show({
-            controller: function DialogController($scope, $mdDialog) {
-                $scope.hide = function () {
-                    $mdDialog.hide();
-                };
-                $scope.cancel = function () {
-                    $mdDialog.cancel();
-                };
-            },
-            templateUrl: 'dialog_rule.html',
-            parent: angular.element(document.body),
-            scope: $scope,
-            preserveScope: true,
-            clickOutsideToClose: true
-        }).then(function (answer) {
-            //$scope.status = 'You said the information was "' + answer + '".';
-            if ($scope.selectedRule.ruleType === "") {
-                delete $scope.selectedRule.ruleType;
-            }
-            $scope.editSkill();
-        }, function () {
-            //$scope.status = 'You cancelled the dialog.';
-            if ($scope.selectedRule.ruleType === "") {
-                delete $scope.selectedRule.ruleType;
-            }
-            $scope.editSkill();
         });
     };
 });
