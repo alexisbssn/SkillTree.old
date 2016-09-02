b64dfdb6fb28b99f78ad545b60a74ed29c249101
alexis.buisson@hotmail.com <alexis.buisson@hotmail.com>
2016-09-02 09:14:59 -0400

    implement jOrbital demo in project

--- demo/userTree/orbits.js
+++ demo/userTree/orbits.js
@@ -1,6 +1,12 @@
 angular.module("skilltree").controller("userTreeController", function ($scope) {
     $scope.createOrbits = function(){
-        
+        $(".orbital4").jOrbital({});
+        $(".orbital3").jOrbital({});
+        $(".orbital2").jOrbital({});
         $(".orbital").jOrbital({});  
     };
+    
+    $scope.$watch('models.jsonRules', function (model) {
+        $scope.createOrbits();
+    }, true);
 });
