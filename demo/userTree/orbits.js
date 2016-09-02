e97d0083c0212a0bf04a2a5ccac415cf9055f1b4
alexis.buisson@hotmail.com <alexis.buisson@hotmail.com>
2016-09-02 11:01:36 -0400

    working with textarea, with data from admin page clipboard

--- demo/userTree/orbits.js
+++ demo/userTree/orbits.js
@@ -1,10 +1,12 @@
 angular.module("skilltree").controller("userTreeController", function ($scope) {
     
     $scope.createOrbits = function(){
+        models = angular.fromJson($scope.textAreaData);
+        
         var root = $(".orbitsRoot");
         root.children().remove();
         var skills = {type: "group", title: "Skills", items: []};
-        skills.items = models.dropzones.Skills;
+        skills.items = models.Skills;
         $scope.createSkillNodes(skills, root);
     };
     
@@ -22,101 +24,7 @@ angular.module("skilltree").controller("userTreeController", function ($scope) {
                 var elem = $("<span class='planet'>" + item.title + "</div>");
                 relativeRoot.append(elem);
             }
-    };
-    
-    //$scope.$watch('models.jsonRules', function (model) {
-    //    $scope.models = angular.fromJson(model);
-    //    $scope.createOrbits();
-    //}, true);
-    
-    //test data, to remove.
-    var models = {
-        selected: null,
-        templates: [
-            {id: "", type: "skill", title: "new skill", description: "", rules: []},
-            {id: "", type: "group", title: "new skill group", items: []},
-            {type: "rule", title: "new rule", target: "", ruleType: "", value: ""}
-        ],
-        ruleTypes: [
-            {data: "", label: "-- Select a rule type --"},
-            {data: "gt", label: "has more than ... taken in target"},
-            {data: "lt", label: "has less than ... taken in target"},
-            {data: "eq", label: "has equal to ... taken in target"},
-            {data: "cst", label: "costs ... target"}
-        ],
-        dropzones: {
-            Skills: [
-                {
-                    id: "1",
-                    type: "group",
-                    title: "Magic",
-                    items: [
-                        {
-                            id: "2",
-                            type: "skill",
-                            title: "Fireball",
-                            description: "2 damage",
-                            rules: []
-                        },
-                        {
-                            id: "3",
-                            type: "skill",
-                            title: "Frostbite",
-                            description: "2 damage",
-                            rules: []
-                        },
-                        {
-                            id: "4",
-                            type: "skill",
-                            title: "Entangle",
-                            description: "2 damage",
-                            rules: []
-                        },
-                        {
-                            id: "5",
-                            type: "skill",
-                            title: "Magic missile",
-                            description: "2 damage",
-                            rules: []
-                        }
-                    ]
-                },
-                {
-                    id: "6",
-                    type: "group",
-                    title: "Combat",
-                    items: [
-                        {
-                            id: "7",
-                            type: "skill",
-                            title: "Parry",
-                            description: "block 2 damage",
-                            rules: []
-                        },
-                        {
-                            id: "8",
-                            type: "skill",
-                            title: "Lunge",
-                            description: "2 damage",
-                            rules: []
-                        },
-                        {
-                            id: "9",
-                            type: "skill",
-                            title: "Slash",
-                            description: "2 damage",
-                            rules: []
-                        }
-                    ]
-                },
-                {
-                    id: "10",
-                    type: "skill",
-                    title: "Health",
-                    description: "2 hp",
-                    rules: []
-                }
-            ]
-        }
-    };
+    };    
+    var models = {};
+    $scope.textAreaData = "";
 });
